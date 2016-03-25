c1a44958a926a2cad09849d498cd67b001092721
山内 卓朗 <yamauchi_takuro@cyberagent.co.jp>
2016-03-25 16:10:38 +0900

    modal追加

--- src/js/all_js/all.js
+++ src/js/all_js/all.js
@@ -1,11 +1,49 @@
 (function() {
 	console.log('common.js');
+
+	var $DOC = $(document);
+
+	var body  = 'body, html';
+	var $body = $(body);
+
+	var pageMain  = '#page-main';
+	var $pageMain = $(pageMain);
+
+	if($pageMain.length > 0) {
+		var listCnt  = '#js-cnt-list';
+		var $listCnt = $(listCnt);
+		var btnCloseModal  = '.js-btn-close-modal';
+		var $btnCloseModal = $(btnCloseModal);
+		var blackLayer  = '.js-black-layer';
+		var $blackLayer = $(blackLayer);
+		var data;
+		var $modal;
+		var $target;
+
+		// modal - open/close
+		$DOC.on('click', listCnt, function() {
+			data    = $(this).attr('data-item');
+			$modal  = $('#' + data);
+			$target = data;
+			$body.addClass('lock');
+			$modal.fadeIn(250);
+			$blackLayer.fadeIn(250);
+			// data-modalに値を設定
+			$blackLayer.attr('data-modal', $target);
+		});
+		$DOC.on('click', btnCloseModal, function() {
+			$(this).closest('.l-content-modal').fadeOut(250);
+			$body.removeClass('lock');
+			$blackLayer.fadeOut(250);
+		});
+		$DOC.on('click', blackLayer, function() {
+			data   = $blackLayer.attr('data-modal');
+			$modal = $('#' + data);
+			$body.removeClass('lock');
+			$modal.fadeOut(250);
+			$blackLayer.fadeOut(250);
+		});
+	}
+
 })();
 
-(function() {
-	console.log('search.js');
-})();
-
-(function() {
-	console.log('top.js');
-})();
